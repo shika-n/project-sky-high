35cf6ae0639062484501bade4e744c6aa4d34ad7
Shika <greffinroid@ymail.com>
2025-11-10 14:52:04 +0900

    Logical device query and a bit of swapchain

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,21 @@
 #include <cstdint>
 #include <cstring>
 #include <exception>
+#include <format>
 #include <functional>
-#include <iterator>
+#include <limits>
 #include <map>
 #include <print>
+#include <ranges>
 #include <stdexcept>
+#include <string>
+#include <vector>
 
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan_core.h>
+#include <vulkan/vulkan_enums.hpp>
 #include <vulkan/vulkan_raii.hpp>
 #include <vulkan/vulkan_structs.hpp>
 
@@ -19,6 +26,12 @@ constexpr uint32_t WIDTH = 1280;
 constexpr uint32_t HEIGHT = 720;
 
 // TODO: Add validation layers
+
+struct DeviceSuitableness {
+	bool is_suitable = false;
+	uint32_t graphics_queue_index = 0;
+	uint32_t present_queue_index = 0;
+};
 
 class ProjectSkyHigh {
 public:
@@ -37,6 +50,13 @@ private:
 	vk::raii::PhysicalDevice physical_device = nullptr;
 	vk::raii::Device device = nullptr;
 	vk::raii::Queue graphics_queue = nullptr;
+	vk::raii::Queue present_queue = nullptr;
+	vk::raii::SurfaceKHR surface = nullptr;
+
+	vk::SurfaceFormatKHR swap_chain_surface_format {};
+	vk::Extent2D swap_chain_extent {};
+
+	DeviceSuitableness device_suitableness {};
 
 	std::vector<char const*> required_device_extensions = {
 		vk::KHRSwapchainExtensionName,
@@ -60,8 +80,10 @@ private:
 
 	void init_vulkan() {
 		create_instance();
+		create_surface();
 		pick_physical_device();
 		create_logical_device();
+		create_swap_chain();
 	}
 
 	void main_loop() {
@@ -117,39 +139,31 @@ private:
 		instance = vk::raii::Instance(context, create_info);
 	}
 
-	uint32_t get_device_score(vk::raii::PhysicalDevice const& device) {
-		uint32_t score = 0;
-		bool api_version_support = false;
-		bool is_discreet = false;
-		bool required_queue_available = true;
-		bool required_extensions_available = true;
+	void create_surface() {
+		VkSurfaceKHR surface_khr;
+		if (glfwCreateWindowSurface(*instance, window, nullptr, &surface_khr) !=
+			VK_SUCCESS) {
+			throw std::runtime_error("Failed to create window surface");
+		}
+		surface = vk::raii::SurfaceKHR(instance, surface_khr);
+	}
+
+	DeviceSuitableness
+	get_device_suitableness(vk::raii::PhysicalDevice device) {
+		bool is_suitable = true;
+		bool is_api_supported = true;
+		std::map<char const*, bool> extension_support_map;
 
 		auto property = device.getProperties();
-		if (property.apiVersion >= VK_API_VERSION_1_3) {
-			api_version_support = true;
-		}
-		if (property.deviceType == vk::PhysicalDeviceType::eDiscreteGpu) {
-			is_discreet = true;
-			score += 1000;
-		} else if (property.deviceType ==
-				   vk::PhysicalDeviceType::eIntegratedGpu) {
-			score += 100;
-		}
 
-		auto queue_families = device.getQueueFamilyProperties();
-		auto const queue_family_iter = std::ranges::find_if(
-			queue_families, [](vk::QueueFamilyProperties const& queue_family) {
-				return (queue_family.queueFlags &
-						vk::QueueFlagBits::eGraphics) !=
-					   static_cast<vk::QueueFlags>(0);
-			}
-		);
-		if (queue_family_iter == queue_families.end()) {
-			required_queue_available = false;
+		if (property.apiVersion < VK_API_VERSION_1_3) {
+			is_suitable = false;
+			is_api_supported = false;
 		}
 
 		auto available_extensions = device.enumerateDeviceExtensionProperties();
 		for (auto const& extension : required_device_extensions) {
+			extension_support_map[extension] = true;
 			auto iter = std::ranges::find_if(
 				available_extensions,
 				[extension](auto const& available_extension) {
@@ -158,37 +172,77 @@ private:
 					);
 				}
 			);
-			required_extensions_available = required_extensions_available &&
-											iter != available_extensions.end();
+			if (iter == available_extensions.end()) {
+				is_suitable = false;
+				extension_support_map[extension] = false;
+			}
 		}
 
-		if (!api_version_support || !required_queue_available ||
-			!required_extensions_available) {
-			score = 0;
+		// TODO: Improve queue query
+		auto queue_families = device.getQueueFamilyProperties();
+		uint32_t graphics_queue_index = queue_families.size();
+		uint32_t present_queue_index = queue_families.size();
+		for (uint32_t i = 0; i < queue_families.size(); ++i) {
+			auto const& queue_family = queue_families.at(i);
+
+			if (graphics_queue_index == queue_families.size() &&
+				(queue_family.queueFlags & vk::QueueFlagBits::eGraphics) !=
+					static_cast<vk::QueueFlags>(0)) {
+				graphics_queue_index = i;
+			}
+
+			if (present_queue_index == queue_families.size() &&
+				device.getSurfaceSupportKHR(i, *surface)) {
+				present_queue_index = i;
+			}
 		}
 
-		DLOG("Device Detected: {}", device.getProperties().deviceName.data());
-		DLOG("> Vulkan API 1.3 support: {}", api_version_support);
-		DLOG("> Discreet GPU: {}", is_discreet);
-		DLOG("> Required Queues Available: {}", required_queue_available);
+		if (graphics_queue_index == queue_families.size() ||
+			present_queue_index == queue_families.size()) {
+			is_suitable = false;
+		}
+
+		DLOG("Device: {}", std::string(property.deviceName));
+		DLOG("> Vulkan API v1.3 Support: {}", is_api_supported);
+		DLOG("> Extensions:");
+		for (auto entry : extension_support_map) {
+			DLOG(">   - {}: {}", entry.first, entry.second ? "Yes" : "No");
+		}
+		DLOG("> Queue:");
 		DLOG(
-			"> Required Extensions Available: {}", required_extensions_available
+			">   Graphics Queue Index: {}",
+			graphics_queue_index == queue_families.size()
+				? "Not Found"
+				: std::to_string(graphics_queue_index)
 		);
-		DLOG("> Score: {}", score);
+		DLOG(
+			">   Present Queue Index: {}",
+			present_queue_index == queue_families.size()
+				? "Not Found"
+				: std::to_string(present_queue_index)
+		);
 
-		return score;
+		return {
+			.is_suitable = is_suitable,
+			.graphics_queue_index = graphics_queue_index,
+			.present_queue_index = present_queue_index
+		};
 	}
 
-	uint32_t get_queue_family(vk::raii::PhysicalDevice device) {
-		auto queue_families = device.getQueueFamilyProperties();
-		auto const queue_family_iter = std::ranges::find_if(
-			queue_families, [](vk::QueueFamilyProperties const& queue_family) {
-				return (queue_family.queueFlags &
-						vk::QueueFlagBits::eGraphics) !=
-					   static_cast<vk::QueueFlags>(0);
-			}
-		);
-		return std::distance(queue_families.begin(), queue_family_iter);
+	uint32_t get_device_score(vk::raii::PhysicalDevice device) {
+		uint32_t score = 0;
+
+		auto property = device.getProperties();
+		if (property.deviceType == vk::PhysicalDeviceType::eDiscreteGpu) {
+			score += 1000;
+		} else if (property.deviceType ==
+				   vk::PhysicalDeviceType::eIntegratedGpu) {
+			score += 200;
+		} else {
+			score += 100;
+		}
+
+		return score;
 	}
 
 	void pick_physical_device() {
@@ -197,17 +251,36 @@ private:
 			throw std::runtime_error("No GPU that supports Vulkan found");
 		}
 
-		std::
-			multimap<uint32_t, vk::raii::PhysicalDevice, std::greater<uint32_t>>
-				device_scores;
-		for (auto const& device : devices) {
+		std::unordered_map<vk::raii::PhysicalDevice*, DeviceSuitableness>
+			device_suitableness_map;
+		std::ranges::for_each(
+			devices, [this, &device_suitableness_map](auto& device) {
+				device_suitableness_map[&device] =
+					get_device_suitableness(device);
+			}
+		);
+
+		auto suitable_devices = std::ranges::filter_view(
+			devices, [&device_suitableness_map](auto& device) {
+				return device_suitableness_map[&device].is_suitable;
+			}
+		);
+
+		std::multimap<
+			uint32_t,
+			vk::raii::PhysicalDevice*,
+			std::greater<uint32_t>>
+			device_scores;
+		for (auto& device : suitable_devices) {
 			uint32_t score = get_device_score(device);
-			device_scores.insert(std::make_pair(score, device));
+			device_scores.insert(std::make_pair(score, &device));
 		}
 
 		for (auto score_pair : device_scores) {
 			if (score_pair.first > 0) {
-				physical_device = score_pair.second;
+				physical_device = *score_pair.second;
+				device_suitableness =
+					device_suitableness_map[score_pair.second];
 				break;
 			}
 		}
@@ -220,10 +293,9 @@ private:
 	}
 
 	void create_logical_device() {
-		uint32_t queue_family_index = get_queue_family(physical_device);
 		float priority = 0.0f;
 		vk::DeviceQueueCreateInfo device_queue_create_info {
-			.queueFamilyIndex = queue_family_index,
+			.queueFamilyIndex = device_suitableness.graphics_queue_index,
 			.queueCount = 1,
 			.pQueuePriorities = &priority,
 		};
@@ -247,7 +319,78 @@ private:
 		};
 
 		device = vk::raii::Device(physical_device, device_create_info);
-		graphics_queue = vk::raii::Queue(device, queue_family_index, 0);
+		graphics_queue = vk::raii::Queue(
+			device, device_suitableness.graphics_queue_index, 0
+		);
+		present_queue =
+			vk::raii::Queue(device, device_suitableness.present_queue_index, 0);
+	}
+
+	void create_swap_chain() {
+		auto surface_capabilities =
+			physical_device.getSurfaceCapabilitiesKHR(surface);
+		std::vector<vk::SurfaceFormatKHR> available_formats =
+			physical_device.getSurfaceFormatsKHR(surface);
+		std::vector<vk::PresentModeKHR> available_present_mode =
+			physical_device.getSurfacePresentModesKHR(surface);
+
+		swap_chain_surface_format =
+			choose_swap_surface_format(available_formats);
+		swap_chain_extent = choose_swap_extent(surface_capabilities);
+		auto min_image_count = std::max(3u, surface_capabilities.minImageCount);
+
+		if (surface_capabilities.maxImageCount > 0 &&
+			min_image_count > surface_capabilities.minImageCount) {
+			min_image_count = surface_capabilities.maxImageCount;
+		}
+	}
+
+	vk::SurfaceFormatKHR choose_swap_surface_format(
+		std::vector<vk::SurfaceFormatKHR> const& available_formats
+	) {
+		for (auto const& format : available_formats) {
+			if (format.format == vk::Format::eB8G8R8A8Srgb &&
+				format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear) {
+				return format;
+			}
+		}
+		return available_formats[0];
+	}
+
+	vk::PresentModeKHR choose_present_mode(
+		std::vector<vk::PresentModeKHR> const& available_present_mode
+	) {
+		for (auto const& mode : available_present_mode) {
+			if (mode == vk::PresentModeKHR::eMailbox) {
+				return mode;
+			}
+		}
+		return vk::PresentModeKHR::eFifo;
+	}
+
+	vk::Extent2D
+	choose_swap_extent(vk::SurfaceCapabilitiesKHR const& capabilities) {
+		if (capabilities.currentExtent.width !=
+			std::numeric_limits<uint32_t>::max()) {
+			return capabilities.currentExtent;
+		}
+
+		int width = 0;
+		int height = 0;
+		glfwGetFramebufferSize(window, &width, &height);
+
+		return {
+			std::clamp<uint32_t>(
+				width,
+				capabilities.minImageExtent.width,
+				capabilities.maxImageExtent.width
+			),
+			std::clamp<uint32_t>(
+				height,
+				capabilities.minImageExtent.height,
+				capabilities.maxImageExtent.height
+			)
+		};
 	}
 };
 
